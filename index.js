ce01cda4f99dd587d3fe85047d2154e356948f20
Aaron Tan <info@aarontan.co.nz>
2022-06-25 20:18:22 +1200

    fix random decision between first and second choice. removed submit button.

--- index.js
+++ index.js
@@ -1,25 +1,40 @@
 let choice = Math.trunc(Math.random() * 2) + 1
 let isTired = document.querySelector('.tired').value
 let notTired = document.querySelector('.notTired').value
-let hideBlock = document.querySelector('.hidden')
 
 document.querySelector('.tired').addEventListener('click', function () {
-  document.querySelector('.block').classList.add('hidden')
-  document.querySelector('.send').addEventListener('click', function () {
-    document.querySelector('.outcomeAnswer').textContent = 'go to sleep'
-  })
+  if (isTired) {
+    document.querySelector('.outcomeAnswer').textContent = ''
+    document.querySelector('.decide').addEventListener('click', function () {
+      document.querySelector('.outcomeAnswer').textContent = 'go to sleep'
+    })
+  }
 })
 
 document.querySelector('.notTired').addEventListener('click', function () {
-  let hideBlock = document.querySelector('.hidden')
-
-  if (hideBlock) {
-    document.querySelector('.hidden').classList.remove('hidden')
+  if (notTired) {
+    document.querySelector('.outcomeAnswer').textContent = ''
     document.querySelector('.decide').addEventListener('click', function () {
-      let choice1 = document.querySelector('.choice1').valuehidden
+      choice = Math.trunc(Math.random() * 2) + 1
+      let choice1 = document.querySelector('.choice1').value
       let choice2 = document.querySelector('.choice2').value
       let result = choice === 1 ? choice1 : choice2
       document.querySelector('.outcomeAnswer').textContent = result
     })
   }
 })
+
+// let isTired = document.querySelector('.tired').value
+// let notTired = document.querySelector('.notTired').value
+// let choice = Math.floor(Math.random() * 2) + 1
+// let blockChoice = document.querySelector('.hidden')
+// blockChoice.classList.add('hidden')
+// if (notTired === true) {
+//   if (choice === 1) {
+//     blockChoice.classList.remove('hidden')
+//   } else {
+//     console.log('watch The Boys')
+//   }
+// } else {
+//   console.log('study')
+// }
